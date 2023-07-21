e1bca5137d57838b1efbb2e5bc33f0ece04d7d11
JDGassiraro <josephgassiraro2021@gmail.com>
2023-07-20 23:57:13 -0500

    SPA Conversion Attempt 1

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+import html from "html-literal";
+
+export default () => html `<footer>
+<p>Created by Joseph 7/19/23</p>
+</footer>`;
---
+++ components/views/Home.js
@@ -0,0 +1,53 @@
+import html from "html-literal";
+
+export default () => html`
+<!--Carousel Bar-->
+<div class="carousel-section">
+    <h3 class="game-section-header">Featured Games</h3>
+    <div class="carousel-games">
+        <a href="gameSpecific.html"><img class="carousel-image" alt="game image"></a>
+        <a href="gameSpecific.html"><img class="carousel-image" alt="game image"></a>
+        <a href="gameSpecific.html"><img class="carousel-image" alt="game image"></a>
+        <a href="gameSpecific.html"><img class="carousel-image" alt="game image"></a>
+        <a href="gameSpecific.html"><img class="carousel-image" alt="game image"></a>
+        <a href="gameSpecific.html"><img class="carousel-image" alt="game image"></a>
+        <a href="gameSpecific.html"><img class="carousel-image" alt="game image"></a>
+        <a href="gameSpecific.html"><img class="carousel-image" alt="game image"></a>
+        <a href="gameSpecific.html"><img class="carousel-image" alt="game image"></a>
+        <a href="gameSpecific.html"><img class="carousel-image" alt="game image"></a>
+    </div>
+</div>
+
+<!--Most Popular Games-->
+<div class="game-section">
+    <h3 class="game-section-header">Most Popular Games this Week</h3>
+    <div class="game-section-list">
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+    </div>
+</div>
+
+<!--All-time Most Popular Games-->
+<div class="game-section">
+    <h3 class="game-section-header">All-time Most Popular Games</h3>
+    <div class="game-section-list">
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+        <a href="gameSpecific.html"><img alt="game image"></a>
+    </div>
+</div>`;
