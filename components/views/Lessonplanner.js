d5985d1aa8831241a451203f51752c962eb2ee6e
JDGassiraro <josephgassiraro2021@gmail.com>
2023-08-12 17:11:20 -0500

    converted AVIF files, updated page content and styling

--- components/views/Lessonplanner.js
+++ components/views/Lessonplanner.js
@@ -1,13 +1,17 @@
 import html from "html-literal";
+import stockURL from "/images/gameImages/diverUnderwater.jpg";
+import bannerImage from "/images/educationImages/apple-4958609_1280.png";
+import schoolImage1 from "/images/educationImages/back-view-adults-studying-together_23-2150331653.png";
+import schoolImage2 from "/images/educationImages/back-view-adults-studying-together_23-2150331653.png";
+
 
 export default () => html `<h1>
-      Lesson Planer Page
+      Lesson Planner Page
 </h1>
-<div>
-      <img class="banner-image" src="images/educationImages/apple-4958609_1280.jpg" alt="banner image">
-</div>
+<div class="passive-banner-image-div">
+      <img class="passive-banner-image" src="${bannerImage}" alt="banner image">
 
-<div class="lp-dropdown">
+      <div class="lp-dropdown">
       <label for="weekplanner">Select Cirriculum Week:</label>
             <select name="weekplanner" id="weekplanner">
                   <option value="Week 1">Week 1</option>
@@ -17,29 +21,38 @@ export default () => html `<h1>
                   <option value="Week 5">Week 5</option>
             </select>
 </div>
+</div>
+
+
 
 <div class="lp-result-info">
-      <img alt="school image">
-            <div>
-                  <h3>Week PlaceHolder Text</h3>
-                  <p>This is the advised games for the cirriculum period selected</p>
+      <div class="lp-image">
+            <img src="${schoolImage1}" alt="school image">
+      </div>
+            <div class="lp-result-text">
+                  <h3 class="center-header">Week PlaceHolder Text</h3>
+                  <p>This area will describe the games below and the best way that they can be used
+                        in a class cirriculum.
+                  </p>
             </div>
-      <img alt="school image">
+      <div class="lp-image">
+            <img src="${schoolImage2}" alt="school image">
+      </div>
 </div>
 
 <div class="game-section">
     <h3 class="game-section-header">Most Popular Games this Week</h3>
     <div class="game-section-list">
-        <a href="Gamespecific"><img alt="game image"></a>
-        <a href="Gamespecific"><img alt="game image"></a>
-        <a href="Gamespecific"><img alt="game image"></a>
-        <a href="Gamespecific"><img alt="game image"></a>
-        <a href="Gamespecific"><img alt="game image"></a>
-        <a href="Gamespecific"><img alt="game image"></a>
-        <a href="Gamespecific"><img alt="game image"></a>
-        <a href="Gamespecific"><img alt="game image"></a>
-        <a href="Gamespecific"><img alt="game image"></a>
-        <a href="Gamespecific"><img alt="game image"></a>
+        <a href="Gamespecific"><img src="${stockURL}" alt="game image"></a>
+        <a href="Gamespecific"><img src="${stockURL}" alt="game image"></a>
+        <a href="Gamespecific"><img src="${stockURL}" alt="game image"></a>
+        <a href="Gamespecific"><img src="${stockURL}" alt="game image"></a>
+        <a href="Gamespecific"><img src="${stockURL}" alt="game image"></a>
+        <a href="Gamespecific"><img src="${stockURL}" alt="game image"></a>
+        <a href="Gamespecific"><img src="${stockURL}" alt="game image"></a>
+        <a href="Gamespecific"><img src="${stockURL}" alt="game image"></a>
+        <a href="Gamespecific"><img src="${stockURL}" alt="game image"></a>
+        <a href="Gamespecific"><img src="${stockURL}" alt="game image"></a>
     </div>
 </div>
 
